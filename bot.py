1f27d52db488063acb86d4bd556e56e8f14aa907
Muhammad Saqlain <154231070+saqlainshabbir@users.noreply.github.com>
2024-06-03 22:01:09 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,68 @@
+import datetime
+from discord.ext import commands, tasks
+import discord
+from dataclasses import dataclass
+
+BOT_TOKEN = "MTI0NjIzMjI0MzA4MTQ0OTUzNA.GaBfGe.HFVe6uTEh2pqqbAfmCIsfUhJqw8HwugWKIojnQ"
+CHANNEL_ID = 1201530281790804021
+MAX_SESSION_TIME_MINUTES = 45
+
+@dataclass
+class Session:
+    is_active: bool = False
+    start_time: int = 0
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+session = Session()
+
+@bot.event
+async def on_ready():
+    print("Hello! Study bot is ready!")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("Hello! Study bot is ready!")
+
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    result = a + b
+    await ctx.send(f"The result of {a} + {b} is {result}.")
+
+
+@tasks.loop(minutes=MAX_SESSION_TIME_MINUTES, count=2)
+async def break_reminder():
+
+    # Ignore the first execution of this command.
+    if break_reminder.current_loop == 0:
+        return
+
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send(f"**Take a break!** You've been studying for {MAX_SESSION_TIME_MINUTES} minutes.")
+
+@bot.command()
+async def start(ctx):
+    if session.is_active:
+        await ctx.send("A session is already active!")
+        return
+
+    session.is_active = True
+    session.start_time = ctx.message.created_at.timestamp()
+    human_readable_time = ctx.message.created_at.strftime("%H:%M:%S")
+    break_reminder.start()
+    await ctx.send(f"New session started at {human_readable_time}")
+
+
+@bot.command()
+async def end(ctx):
+    if not session.is_active:
+        await ctx.send("No session is active!")
+        return
+
+    session.is_active = False
+    end_time = ctx.message.created_at.timestamp()
+    duration = end_time - session.start_time
+    human_readable_duration = str(datetime.timedelta(seconds=duration))
+    break_reminder.stop()
+    await ctx.send(f"Session ended after {human_readable_duration}.")
+
+
+bot.run(BOT_TOKEN)
